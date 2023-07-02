ef4a9367330276056b17a67fcf1133b60a86bece
Adri Silveira <73858311+AdriSilveira@users.noreply.github.com>
2023-07-02 15:02:06 +0100

    Update Navbar.jsx
    Change in the navbar module page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,32 +1,26 @@
-import { NavLink } from 'react-router-dom';
-import './Navbar.scss';
+import { NavLink } from "react-router-dom";
+import "./Navbar.scss";
 
 function Navbar() {
-
   return (
     <nav>
       <div className="navItem">
         <NavLink to="/fauxLogin">Faux-Lo</NavLink>
-      </div>      
-      
-<<<<<<< Updated upstream
+      </div>
+
       {/* <div className="navItem">
         <NavLink to="/">Home</NavLink>
       </div> */}
-      
+
       <div className="navItem">
         <NavLink to="/modules">Modules</NavLink>
-=======
-      <div className="navItem">
-        <NavLink to="/modules">Home</NavLink>
->>>>>>> Stashed changes
       </div>
-      
+
       <div className="navItem">
         <NavLink to="/students">Students</NavLink>
       </div>
     </nav>
-  );  
+  );
 }
 
 export default Navbar;

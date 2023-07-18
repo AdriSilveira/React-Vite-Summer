04eb8881086d4c7bd27ac6783a04b4eba0423fb2
Tom Anderson <tom94anderson@gmail.com>
2023-07-18 14:28:22 +0100

    Global Groups Page adjustments

--- src/components/views/Groups.jsx
+++ src/components/views/Groups.jsx
@@ -1,8 +1,66 @@
-function Groups() {
+import React, { useState, useEffect } from 'react';
+import { CardContainer, Card } from '../UI/Card.jsx';
+import './Groups.scss';
+
+function Groups({ setIsModulesView }) {
+  // State for storing the groups
+  const [groups, setGroups] = useState([]);
+
+  // Function to handle going back to the Modules view
+  const selectModule = () => {
+    setIsModulesView(true);
+  };
+
+  // Funciton to toggle the dropdown of a group
+  const toggleGroupDropdown = (groupId) => {
+    setGroups((prevGroups) => {
+      const updatedGroups = [...prevGroups];
+      const groupIndex = updatedGroups.findIndex((group) => group.id === groupId);
+      updatedGroups[groupIndex].isOpen = !updatedGroups[groupIndex].isOpen;
+      return updatedGroups;
+    });
+  };
+
+  // Function to fetch the groups (manual for now until endpoints added)
+  const fetchGroups = () => {
+    const fetchedGroups = [
+      { id: 1, name: 'Group Name 1', members: ['Student 1', 'Student 2', 'Student 3'], isOpen: false },
+      { id: 2, name: 'Group Name 2', members: ['Student 4', 'Student 5'], isOpen: false },
+      { id: 3, name: 'Group Name 3', members: ['Student 6', 'Student 7', 'Student 8', 'Student 9'], isOpen: false },
+    ];
+
+    setGroups(fetchedGroups);
+  };
+
+  // Fetch the groups on component mount
+  useEffect(() => {
+    fetchGroups();
+  }, []);
+
   return (
     <>
-      <h1>ALL GROUP PAGES</h1>
-      <h2>Sorry....Page in progress</h2>
+      <CardContainer>
+        <h1>Global Groups</h1>
+      </CardContainer>
+      <Card>
+        <CardContainer>
+          {groups.map((group) => (
+            <div className="groupCard" key={group.id}>
+              <div className="groupHeader" onClick={() => toggleGroupDropdown(group.id)}>
+                <h3>{group.name}</h3>
+              </div>
+              {group.isOpen && (
+                <div className="groupDropdown">
+                  {group.members.map((member, index) => (
+                    <p key={index}>{member}</p>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </CardContainer>
+      </Card>
+      <button onClick={selectModule}>Back to Modules</button>
     </>
   );
 }

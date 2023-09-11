1140b2823d36aa9340febcffc1c67082db453d11
Adri Silveira <73858311+AdriSilveira@users.noreply.github.com>
2023-09-11 13:45:03 +0100

    SCSS update

--- src/components/views/GroupInfo.jsx
+++ src/components/views/GroupInfo.jsx
@@ -1,44 +1,53 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { CardContainer, Card } from "../UI/Card.jsx";
 import UserCard from "../Entity/User/UserCard.jsx";
 import LogForm from "../Entity/Logs/LogForm.jsx";
 import "./Modules.scss";
-import "../Entity/Logs/LogForm.jsx";
 import "./GroupInfo.scss";
 
 function GroupInfo({ selectedGroupID }) {
-  // Initialisation --------------------------------------
-  //adding a button
-  const RoundButton = () => {
-    return <button classname="actions"></button>;
-  };
-  console.log(selectedGroupID);
   const SelectedGroup = selectedGroupID;
   const apiURL = "http://softwarehub.uk/unibase/api";
   const myGroupEndpoint = `${apiURL}/users/groups/${SelectedGroup}`;
+  const logsEndpoint = `${apiURL}/logs`; // API endpoint for fetching logs
 
-  // State ---------------------------------------
   const [GroupStudents, setGroupStudents] = useState(null);
+  const [showLogForm, setShowLogForm] = useState(false);
+  const [logsrec, setLogsrec] = useState([]);
 
-  const [showCoLoForm, setShowCoLoForm] = useState(false);
+  const fetchGroupStudents = async () => {
+    try {
+      const response = await fetch(myGroupEndpoint);
+      if (!response.ok) {
+        throw new Error("Failed to fetch group students");
+      }
+      const data = await response.json();
+      setGroupStudents(data);
+    } catch (error) {
+      console.error("Error fetching group students: ", error);
+    }
+  };
 
-  const apiGet = async (endpoint) => {
-    const response = await fetch(endpoint);
-    const result = await response.json();
-    setGroupStudents(result);
+  const fetchLogs = async () => {
+    try {
+      const response = await fetch(logsEndpoint);
+      if (!response.ok) {
+        throw new Error("Failed to fetch logs");
+      }
+      const data = await response.json();
+      setLogsrec(data);
+    } catch (error) {
+      console.error("Error fetching logs: ", error);
+    }
   };
 
   useEffect(() => {
-    apiGet(myGroupEndpoint);
-  }, [myGroupEndpoint]);
+    fetchGroupStudents();
+    fetchLogs();
+  }, [selectedGroupID]);
 
-  // Handlers ------------------------------------
-  // Function to handle going back to the Modules view
-  const selectGroup = () => {
-    setIsGroupView(true);
-  };
+  const [isGroupView, setIsGroupView] = useState(false);
 
-  // View --------------------------------------
   return (
     <>
       {!GroupStudents ? (
@@ -53,24 +62,28 @@ function GroupInfo({ selectedGroupID }) {
           <Card>
             <CardContainer>
               {GroupStudents.map((students) => (
-                <UserCard
-                  user={students}
-                  //key={students.UserID}
-                />
+                <UserCard key={students.UserID} user={students} />
               ))}
             </CardContainer>
           </Card>
           <CardContainer>
+            <h2>Logs</h2>
+            <ul>
+              {logsrec.map((log) => (
+                <li key={log.id}>{log.message}</li>
+              ))}
+            </ul>
+          </CardContainer>
+          <CardContainer>
             <div className="button-CoLo">
-              <button onClick={() => setShowCoLoForm(true)}>
-                {" "}
-                Add Contribution Log{" "}
+              <button onClick={() => setShowLogForm(true)}>
+                Add Contribution Log
               </button>
             </div>
-            {showCoLoForm && (
+            {showLogForm && (
               <LogForm
-                onCancel={() => setShowCoLoForm(false)}
-                onSuccess={() => setShowCoLoForm(false)}
+                onCancel={() => setShowLogForm(false)}
+                onSuccess={() => setShowLogForm(false)}
               />
             )}
           </CardContainer>

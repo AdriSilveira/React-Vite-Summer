8abefe9744639b6df23b71453ecb0db8164b8667
George Carter <geco813@gmail.com>
2023-09-20 15:09:20 +0100

    fixed grid

--- src/components/Entity/Logs/LogCard.jsx
+++ src/components/Entity/Logs/LogCard.jsx
@@ -9,54 +9,56 @@ import { useEffect, useState } from "react";
 function LogCard({ log, contributions, students }) {
   // Initialisation --------------------------------------
   // State -----------------------------------------------
-  const [sortedContribution, setSortedContribuion] = useState([]);
+  //const [sortedContribution, setSortedContribuion] = useState([]);
 
   const [completeContribution, setCompleteContribution] = useState([]);
 
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    sortContributions(contributions);
-    finalContributions(sortedContribution);
-  }, [contributions, sortedContribution]);
+    sortContributions(contributions, students);
+  }, [contributions, students]);
 
-  const sortContributions = (contributions) => {
-    if (contributions.length == 1) {
-      setSortedContribuion(contributions);
-    } else {
-      setSortedContribuion(
-        contributions.sort(function (a, b) {
-          return a.ContributionUserID - b.ContributionUserID;
-        })
-      );
-    }
-  };
-  const finalContributions = (sortedContribution) => {
+  useEffect(() => {
+    console.log("completeContribution");
+    console.log(completeContribution);
+  }, [completeContribution]);
+
+  const sortContributions = (contributions, students) => {
+    const sortedContribution = contributions.sort(function (a, b) {
+      return a.ContributionUserID - b.ContributionUserID;
+    });
     console.log("--------");
     console.log(contributions);
     console.log(sortedContribution);
+
+    const tempCont = [];
 
     students.forEach((student) => {
       console.log(student.UserID);
       //console.log(sortedContribution[count].ContributionUserID);
       if (sortedContribution[count] == undefined) {
-        const tempCont = [completeContribution, EmptyContribution[0]];
+        tempCont.push(EmptyContribution[0]);
         setCompleteContribution(tempCont);
         console.log("a");
+        console.log(tempCont);
       } else if (
         student.UserID == sortedContribution[count].ContributionUserID
       ) {
-        const tempCont = [completeContribution, sortedContribution[count]];
+        tempCont.push(sortedContribution[count]);
         setCompleteContribution(tempCont);
         setCount(count + 1);
+        console.log(sortedContribution[count]);
         console.log("b");
+        console.log(tempCont);
       } else {
-        const tempCont = [completeContribution, EmptyContribution[0]];
+        tempCont.push(EmptyContribution[0]);
         setCompleteContribution(tempCont);
         console.log("c");
       }
       console.log(completeContribution);
     });
+    setCompleteContribution(tempCont);
   };
   // Handlers --------------------------------------------
 

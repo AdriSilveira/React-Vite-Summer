0bd0869b795127f8960f94ea68a99a1923434a2c
George Carter <46428450+BossName2@users.noreply.github.com>
2023-09-20 14:08:32 +0000

    Grid displaying logs now works

--- src/components/Entity/Logs/LogCard.jsx
+++ src/components/Entity/Logs/LogCard.jsx
@@ -9,57 +9,56 @@ import { useEffect, useState } from "react";
 function LogCard({ log, contributions, students }) {
   // Initialisation --------------------------------------
   // State -----------------------------------------------
-  const [sortedContribution, setSortedContribuion] = useState([]);
+  //const [sortedContribution, setSortedContribuion] = useState([]);
 
   const [completeContribution, setCompleteContribution] = useState([]);
 
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCompleteContribution([]);
-    sortContributions(contributions);
-    finalContributions(sortedContribution);
-  }, [contributions]);
+    sortContributions(contributions, students);
+  }, [contributions, students]);
 
-  const sortContributions = (contributions) => {
-    if (contributions.length == 1) {
-      setSortedContribuion(contributions);
-    } else {
-      setSortedContribuion(
-        contributions.sort(function (a, b) {
-          return a.ContributionUserID - b.ContributionUserID;
-        })
-      );
-    }
-  };
-  const finalContributions = (sortedContribution) => {
+  useEffect(() => {
+    console.log("completeContribution");
+    console.log(completeContribution);
+  }, [completeContribution]);
+
+  const sortContributions = (contributions, students) => {
+    const sortedContribution = contributions.sort(function (a, b) {
+      return a.ContributionUserID - b.ContributionUserID;
+    });
     console.log("--------");
     console.log(contributions);
     console.log(sortedContribution);
+
+    const tempCont = [];
 
     students.forEach((student) => {
       console.log(student.UserID);
       //console.log(sortedContribution[count].ContributionUserID);
       if (sortedContribution[count] == undefined) {
-        const tempContA = completeContribution.concat(EmptyContribution[0]);
-        setCompleteContribution(tempContA);
+        tempCont.push(EmptyContribution[0]);
+        setCompleteContribution(tempCont);
         console.log("a");
+        console.log(tempCont);
       } else if (
         student.UserID == sortedContribution[count].ContributionUserID
       ) {
-        const tempContB = completeContribution.concat(
-          sortedContribution[count]
-        );
-        setCompleteContribution(tempContB);
+        tempCont.push(sortedContribution[count]);
+        setCompleteContribution(tempCont);
         setCount(count + 1);
+        console.log(sortedContribution[count]);
         console.log("b");
+        console.log(tempCont);
       } else {
-        const tempContC = completeContribution.concat(EmptyContribution[0]);
-        setCompleteContribution(tempContC);
+        tempCont.push(EmptyContribution[0]);
+        setCompleteContribution(tempCont);
         console.log("c");
       }
       console.log(completeContribution);
     });
+    setCompleteContribution(tempCont);
   };
   // Handlers --------------------------------------------
 
@@ -88,3 +87,4 @@ LogCard.propTypes = {
 };
 
 export default LogCard;
+
